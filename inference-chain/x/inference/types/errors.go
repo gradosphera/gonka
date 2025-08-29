c2e5bc18f646c45f0451728ce8557e7425013227
johnlong <john.long@productscience.ai>
2025-08-29 14:42:14 -0700

    Fix possible panics for negative coins `sdk.NewInt64Coin` WILL panic if it gets a negative value. Add error checking for every instance where we use NewInt64Coin (minus a few in Genesis we can ignore) Add a Safe method for logging subaccount transactions to reduce the (now) needed boilerplate Move from using uint64 in our interior methods. In the future, we need to strictly use uint64 at the edges (messages, apis) only. The risk of 0 - 1 = uint64.Max is real, and Go idioms avoid uints in general.

--- inference-chain/x/inference/types/errors.go
+++ inference-chain/x/inference/types/errors.go
@@ -47,4 +47,5 @@ var (
 	ErrEpochNotFound                           = sdkerrors.Register(ModuleName, 1136, "epoch not found")
 	ErrIllegalState                            = sdkerrors.Register(ModuleName, 1137, "illegal state for the operation requested")
 	ErrInvalidValidationThreshold              = sdkerrors.Register(ModuleName, 1138, "validation threshold must be in [0, 100] range")
+	ErrNegativeRewardAmount                    = sdkerrors.Register(ModuleName, 1139, "negative reward amount")
 )
