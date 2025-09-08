a9ea2fe505e791870c44cc7b82f5c8d5ae7609b6
dima <dima.orekhov@productscience.ai>
2025-09-08 14:24:31 -0700

    prepare

--- test-net-cloud/nebius/prepare.sh
+++ test-net-cloud/nebius/prepare.sh
@@ -1,4 +1,2 @@
-scp genesis.py ubuntu@89.169.111.79:~/.
-scp genesis-overrides.json ubuntu@89.169.111.79:~/.
-scp genesis.py ubuntu@89.169.110.61:~/.
-scp join-1.sh ubuntu@89.169.110.61:~/.
+scp genesis.py genesis-overrides.json ubuntu@89.169.111.79:~/
+scp genesis.py join-1.sh ubuntu@89.169.110.61:~/
